35b13cec7bffed1f26391cb6e9991cee85e4ee4c
Edwin Karanu Muiruri <edwin.karanu.muiruri@gmail.com>
2020-06-09 02:56:51 +0300

    Edited the project upload form.

--- ratingapp/forms.py
+++ ratingapp/forms.py
@@ -5,7 +5,7 @@ from django import forms
 class ProjectUploadForm(ModelForm):
     class Meta:
         model = Project
-        exclude = ['profile','content_rating','design_rating','usability_rating','average_content_rating','average_design_rating','average _usability_rating','average_rating']
+        exclude = ['profile','content_rating','design_rating','usability_rating','average_content_rating','average_design_rating','average_usability_rating','average_rating','voters']
 
 class RateProjectForm(forms.Form):
     content = forms.IntegerField(min_value = 1, max_value = 10)
