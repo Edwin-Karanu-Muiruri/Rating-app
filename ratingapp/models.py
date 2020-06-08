fb3a775027bcc818ab994aee3455e9ffc6d56ee9
Edwin Karanu Muiruri <edwin.karanu.muiruri@gmail.com>
2020-06-08 06:47:32 +0300

    Created the project model's methods for init instance,save,delete,displaying all projects and searching for projects. Also defined the get_user_projects class method.

--- ratingapp/models.py
+++ ratingapp/models.py
@@ -45,3 +45,23 @@ class Project(models.Model):
     project_description = models.TextField()
     project_url = models.CharField(max_length = 300)
 
+    def __str__(self):
+        return self.title
+
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
+
+    @classmethod
+    def display_all_projects(cls):
+        return cls.objects.all()
+
+    @classmethod 
+    def search_project(cls,name):
+        return Project.objects.filter(title__icontains = name)
+
+    @classmethod
+    def get_user_projects(cls,profile):
+        return cls.objects.filter(profile=profile)
